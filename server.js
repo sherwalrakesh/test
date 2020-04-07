c957758ce6c4d8a76ed3510c615158e0a183ee17
sherwalrakesh <rakesh.sherwal20@gmail.com>
2020-04-07 13:08:07 +0530

    test 2 commit.

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   const { headers, method, url } = req;
   console.log(headers, method, url);
   res.end();
-})
+});
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
