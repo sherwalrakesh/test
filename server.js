94a7b620045bf5563ccf486f231c85c6fb4269ba
sherwalrakesh <rakesh.sherwal20@gmail.com>
2020-04-07 13:02:19 +0530

    first commit.

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+  const { headers, method, url } = req;
+  console.log(headers, method, url);
+  res.end();
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
